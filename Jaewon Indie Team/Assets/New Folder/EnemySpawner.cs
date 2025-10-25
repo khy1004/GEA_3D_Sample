f250f3196e9d2313af6bcbddfef3f11ff742fc0e
khy1004 <khyseog08@gmail.com>
2025-10-26 01:11:57 +0900

    맵 수정

--- Jaewon Indie Team/Assets/New Folder/EnemySpawner.cs
+++ Jaewon Indie Team/Assets/New Folder/EnemySpawner.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-
+    public GameObject[] enemyPrefab;
     public float spawnInterval = 3f;
-
-    public float spawnRange = 5f;
+    public float SpawnRange = 5f;
 
     private float timer = 0f;
-    // Start is called before the first frame update
+    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
 
-        if(timer >= spawnInterval)
+        if (timer >= spawnInterval)
         {
             Vector3 spawnPos = new Vector3(
-                transform.position.x + Random.Range(-spawnRange, spawnRange),
+                transform.position.x + Random.Range(-SpawnRange, SpawnRange),
                 transform.position.y,
-                transform.position.z + Random.Range(-spawnRange, spawnRange)
+                transform.position.z + Random.Range(-SpawnRange, SpawnRange)
                 );
 
-            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            int randomIndex = Random.Range(0, enemyPrefab.Length);
+            Instantiate(enemyPrefab[randomIndex], spawnPos, Quaternion.identity);
             timer = 0f;
         }
     }
-
-    // Update is called once per frame
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(spawnRange * 2, 0.1f, spawnRange * 2));
+        Gizmos.DrawWireCube(transform.position, new Vector3(SpawnRange * 2, 0.1f, SpawnRange * 2));
     }
+
+
+    // Update is called once per frame
+   
 }
